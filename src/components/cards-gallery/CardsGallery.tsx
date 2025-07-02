175162011ed7ec5b9142a27fda5b6159b30bbd98
Xelo04 <aleksnader.wanot@gmail.com>
2025-07-02 16:35:41 +0200

    Refactor CardsGallery layout for improved structure; adjust PhotoCard width for consistency

--- src/components/cards-gallery/CardsGallery.tsx
+++ src/components/cards-gallery/CardsGallery.tsx
@@ -15,14 +15,18 @@ const CardsGallery = ({ title, data }: CardsGalleryProps) => {
   return (
     <div className="cards-gallery">
       <h3>{title}</h3>
-      {data.map((item) => (
-        <PhotoCard
-          key={item.id}
-          src={item.link}
-          title={item.title}
-          date={item.date}
-        />
-      ))}
+      <div className="cards-container">
+        <div className="cards">
+          {data.map((item) => (
+            <PhotoCard
+              key={item.id}
+              src={item.link}
+              title={item.title}
+              date={item.date}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
