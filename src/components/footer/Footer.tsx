b0861050a19dde3b83d8caf0601d9cd35ed981d4
Xelo04 <aleksnader.wanot@gmail.com>
2025-07-02 14:00:50 +0200

    Refactor Footer component and styles; add contact links and improve layout

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,49 @@
+import { Link } from "react-router-dom";
 import "./footer.scss";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <footer className="oregano-regular">
+      <div className="footer-header">
+        <div className="quick-contact">
+          <span className="title">Napisz do mnie</span>
+          <a href="tel:+48123456789">123456789</a>
+          <a href="https://www.instagram.com/fillscott/" target="blank">
+            @Fillscott
+          </a>
+          <a href="mailto:example@email.com">Email@email.com</a>
+        </div>
+        <ul className="nav-links">
+          <Link to="/">
+            <li>
+              <span>STRONA GŁÓWNA</span>
+            </li>
+          </Link>
+          <Link to="/about">
+            <li>
+              <span>MOJA HISTORIA</span>
+            </li>
+          </Link>
+          <Link to="/gallery">
+            <li>
+              <span>GALERIA</span>
+            </li>
+          </Link>
+          <Link to="/contact">
+            <li>
+              <span>KONTAKT</span>
+            </li>
+          </Link>
+        </ul>
+      </div>
+      <div className="footer-info">
+        <span>Flip Scott Skotarczak © 2025</span>
+        <span>
+          Site by <a href="https://github.com/Xelo04">Xelo04</a>
+        </span>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
