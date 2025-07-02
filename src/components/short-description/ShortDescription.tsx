260553b89be009d64010871fbd596c9ac8105472
Xelo04 <aleksnader.wanot@gmail.com>
2025-07-02 15:10:20 +0200

    Added tatoos and refactor code to use components

---
+++ src/components/short-description/ShortDescription.tsx
@@ -0,0 +1,15 @@
+import "./shortDescription.scss";
+
+const ShortDescription = () => {
+  return (
+    <div className="short-description">
+      <h2>Warto walczyć o swoje marzenia!</h2>
+      <p>
+        W przyszłości chcę otworzyć własne studio, które mam nadzieję zaszczyci
+        mnie Twoją obecnością!
+      </p>
+    </div>
+  );
+};
+
+export default ShortDescription;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,12 @@
 import "./home.scss";
 import MainHome from "../../components/main-home/MainHome";
+import ShortDescription from "../../components/short-description/ShortDescription";
 
 const Home = () => {
   return (
     <div className="home">
       <MainHome />
-      <section className="short-description">
-        <h2>Warto walczyć o swoje marzenia!</h2>
-        <p>
-          W przyszłości chcę otworzyć własne studio, które mam nadzieję
-          zaszczyci mnie Twoją obecnością!
-        </p>
-      </section>
+      <ShortDescription />
     </div>
   );
 };
