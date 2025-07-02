a9659794b9844dea574eb35608e44075137e786b
Xelo04 <aleksnader.wanot@gmail.com>
2025-07-02 14:47:07 +0200

    Add main section to Home Page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,25 @@
 import "./home.scss";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <section className="main-home">
+        <div className="writings">
+          <h3>Cześć, jestem</h3>
+          <h1>SCOTT</h1>
+          <p>
+            Jestem pełnym pasji i energii artystą, który uwielbia tworzyć i
+            projektować tatuaże wspólnie z klientami.
+          </p>
+          <span className="corinthia-regular">Filip Skotarczak</span>
+        </div>
+        <div className="image">
+          <img src="/scott-photos/main-no-bg-2.png" alt="" />
+        </div>
+      </section>
+      <h2>Warto walczyć o swoje marzenia!</h2>
+    </div>
+  );
 };
 
 export default Home;
